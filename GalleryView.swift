4d978c2dc6042c3f7f4e8d6e9ec6ea37ce3a4e5f
Sam Burkhard <samryanburk@icloud.com>
2023-04-04 20:28:24 -0400

    Added session configuration and addAnchor callback

--- GalleryView.swift
+++ GalleryView.swift
@@ -10,27 +10,44 @@ import RealityKit
 import ARKit
 
 // MARK: Maybe keep as seperate class for handling certain events?
-class GalleryView: ARView {
+class GalleryView: ARView, ARSessionDelegate {
     
     required init(frame: CGRect) {
         
         #if targetEnvironment(simulator)
             super.init(frame: .zero)
         #else
-            super.init(frame: frame, cameraMode: .ar, automaticallyConfigureSession: true)
+            super.init(frame: frame, cameraMode: .ar, automaticallyConfigureSession: false)
         #endif
         
         let mainAnchor = AnchorEntity(.plane(.vertical, classification: .wall, minimumBounds: .zero))
+        mainAnchor.name = "MainWallAnchor"
         
         arView.renderOptions = .disableAREnvironmentLighting
         arView.renderOptions = .disableMotionBlur
         
+        arView.session.delegate = self
+        
         arView.scene.anchors.append(mainAnchor)
+        
+        let sessionConfiguration = ARWorldTrackingConfiguration()
+        sessionConfiguration.planeDetection = [.vertical]
+        sessionConfiguration.frameSemantics = .personSegmentationWithDepth
+        
+        arView.session.run(sessionConfiguration, options: .resetTracking)
         
     }
     
     required init?(coder decoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        if arView.scene.anchors.contains(where: {
+            $0.name == "MainWallAnchor"
+        }) {
+            print("Added anchor")
+        }
     }
     
     var arView: ARView { return self }
