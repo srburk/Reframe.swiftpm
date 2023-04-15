1c0da87e57b1daf251567d0424a7e02495aaabe9
Sam Burkhard <samryanburk@icloud.com>
2023-04-15 18:31:54 -0400

    Unnecessary extensions removed

--- Extensions.swift
+++ Extensions.swift
@@ -21,31 +21,3 @@ extension ModelComponent {
 extension Color {
     public static let lightGray = Color(red: 236/255, green: 236/255, blue: 237/255)
 }
-
-enum AsyncError: Error {
-    case finishedWithoutValue
-}
-
-extension AnyPublisher {
-    func async() async throws -> Output {
-        try await withCheckedThrowingContinuation { continuation in
-            var cancellable: AnyCancellable?
-            var finishedWithoutValue = true
-            cancellable = first()
-                .sink { result in
-                    switch result {
-                    case .finished:
-                        if finishedWithoutValue {
-                            continuation.resume(throwing: AsyncError.finishedWithoutValue)
-                        }
-                    case let .failure(error):
-                        continuation.resume(throwing: error)
-                    }
-                    cancellable?.cancel()
-                } receiveValue: { value in
-                    finishedWithoutValue = false
-                    continuation.resume(with: .success(value))
-                }
-        }
-    }
-}
